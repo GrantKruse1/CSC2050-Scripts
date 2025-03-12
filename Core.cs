38988d4e7765b7906c6b8336740f68fba219f4d1
GrantKruse1 <grant.kruse@cuw.edu>
2025-03-12 00:10:46 -0500

    Create Core.cs

---
+++ Core.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Core
+{
+    public static Player thePlayer;
+    public static bool westDoor = false;
+    public static bool northDoor = false;
+    public static bool eastDoor = false;
+    public static bool southDoor = false;
+}
