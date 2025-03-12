3ae5a1375008eb70f5ec46339a92343778738b46
GrantKruse1 <grant.kruse@cuw.edu>
2025-03-12 00:12:02 -0500

    Create Exit.cs

---
+++ Exit.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Exit
+{
+    private string direction;
+    Room destination;
+
+    public Exit(string direction, Room destination)
+    {
+        this.direction = direction;
+        this.destination = destination;
+    }
+
+    public string getDirection()
+    {
+        return this.direction;
+    }
+
+    internal Room getDestination()
+    {
+        throw new NotImplementedException();
+    }
+}
