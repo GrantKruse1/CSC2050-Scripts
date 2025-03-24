dda0cd4254f8dabb449f682baf2500af8dc856c3
GrantKruse1 <grant.kruse@cuw.edu>
2025-03-24 10:50:28 -0500

    Update Inhabitant.cs

--- Inhabitant.cs
+++ Inhabitant.cs
@@ -2,16 +2,34 @@ using UnityEngine;
 
 public abstract class Inhabitant
 {
-    public int currHp;
-    public int maxHp;
-    public int ac;
-    public string name;
+    protected int currHp;
+    protected int maxHp;
+    protected int ac;
+    protected string name;
+
 
     public Inhabitant(string name)
     {
+
         this.name = name;
         this.maxHp = Random.Range(30, 50);
         this.currHp = this.maxHp;
         this.ac = Random.Range(10, 20);
+    }
+    public string getName()
+    {
+        return this.name();
+    }
+    public int getCurrHp()
+    {
+        return this.currHp;
+    }
+    public int getMaxHp()
+    {
+        return this.maxHp;
+    }
+    public int getAc()
+    {
+        return this.ac;
     }
 }
