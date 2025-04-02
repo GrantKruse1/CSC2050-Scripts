6794bf1e03703f7763744d6cc6dfe104a7757900
GrantKruse1 <grant.kruse@cuw.edu>
2025-04-02 10:40:12 -0500

    Update Inhabitant.cs

--- Inhabitant.cs
+++ Inhabitant.cs
@@ -7,28 +7,39 @@ public abstract class Inhabitant
     protected int ac;
     protected string name;
 
-
     public Inhabitant(string name)
     {
-
         this.name = name;
         this.maxHp = Random.Range(30, 50);
         this.currHp = this.maxHp;
-        this.ac = Random.Range(10, 20);
+        this.ac = Random.Range(5, 12);
     }
+
     public string getName()
     {
-        return this.name();
+        return this.name;
     }
+
     public int getCurrHp()
     {
         return this.currHp;
     }
+
     public int getMaxHp()
     {
         return this.maxHp;
     }
-    public int getAc()
+
+    public bool isDead()
+    {
+        return this.currHp <= 0;
+    }
+
+    public void takeDamage(int damage)
+    {
+        this.currHp -= damage;
+    }
+    public int getAC()
     {
         return this.ac;
     }
